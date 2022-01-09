c4ce92308a25a2a1b4e5135d2f22e211aa002415
Mykhailo Pechkurov <mihasyk1@gmail.com>
2022-01-09 22:59:22 +0100

    Initial commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Misha Pechkurov on 09.01.22.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
