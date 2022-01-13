39ac34f30ab797ea274c9bc9aba60120b3124115
Mykhailo Pechkurov <mihasyk1@gmail.com>
2022-01-13 23:24:15 +0100

    Bild Groups of View

---
+++ Scrumdinger/MeetingView.swift
@@ -0,0 +1,32 @@
+//
+//  ContentView.swift
+//  Scrumdinger
+//
+//  Created by Misha Pechkurov on 09.01.22.
+//
+
+import SwiftUI
+
+struct MeetingView: View {
+    var body: some View {
+        VStack {
+            ProgressView(value: 5, total: 15)
+            HStack {
+                VStack {
+                    Text("Second elapsed")
+                    Label("300", systemImage: "hourglass.bottomhalf.fill")
+                }
+                VStack {
+                    Text("Second remaining")
+                    Label("600", systemImage: "hourglass.tophalf.fill")
+                }
+            }
+        }
+    }
+}
+
+struct MeetingView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeetingView()
+    }
+}
