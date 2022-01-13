beb03b31947a28a72e7bb2affad2091ab1728ab4
Mykhailo Pechkurov <mihasyk1@gmail.com>
2022-01-13 23:34:11 +0100

    3. Modify and Style Views

--- Scrumdinger/MeetingView.swift
+++ Scrumdinger/MeetingView.swift
@@ -12,16 +12,26 @@ struct MeetingView: View {
         VStack {
             ProgressView(value: 5, total: 15)
             HStack {
-                VStack {
-                    Text("Second elapsed")
+                VStack(alignment: .leading) {
+                    Text("Second elapsed").font(.caption)
                     Label("300", systemImage: "hourglass.bottomhalf.fill")
                 }
-                VStack {
-                    Text("Second remaining")
+                Spacer()
+                VStack(alignment: .trailing) {
+                    Text("Second remaining").font(.caption)
                     Label("600", systemImage: "hourglass.tophalf.fill")
                 }
             }
+            Circle().strokeBorder(lineWidth: 24)
+            HStack {
+                Text("Speaker 1 of 3")
+                Spacer()
+                Button(action: {}){
+                    Image(systemName: "forward.fill")
+                }
+            }
         }
+        .padding()
     }
 }
 
