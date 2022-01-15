b3c3b5885161eaccd1bc2480f4b0e81731489366
Mykhailo Pechkurov <mihasyk1@gmail.com>
2022-01-15 23:43:35 +0100

    Created a card view

--- Scrumdinger/Models/DailyScrum.swift
+++ Scrumdinger/Models/DailyScrum.swift
@@ -10,15 +10,15 @@ import Foundation
 struct DailyScrum {
     var title : String
     var attendees : [String]
-    var lenghtInMinutes : Int
+    var lengthInMinutes : Int
     var theme : Theme
 }
 
 extension DailyScrum {
     static let sampleData: [DailyScrum] =
     [
-        DailyScrum(title: "Design", attendees: ["One", "Two", "Three", "Four"], lenghtInMinutes: 10, theme: .yellow),
-        DailyScrum(title: "App Dev", attendees: ["One", "Two", "Three", "Four"], lenghtInMinutes: 5, theme: .orange),
-        DailyScrum(title: "Web Dev", attendees: ["Chella", "Chris", "Christina", "Eden", "Karla", "Lindsey", "Aga", "Chad", "Jenn", "Sarah"], lenghtInMinutes: 10, theme: .poppy)
+        DailyScrum(title: "Design", attendees: ["One", "Two", "Three", "Four"], lengthInMinutes: 10, theme: .yellow),
+        DailyScrum(title: "App Dev", attendees: ["One", "Two", "Three", "Four"], lengthInMinutes: 5, theme: .orange),
+        DailyScrum(title: "Web Dev", attendees: ["Chella", "Chris", "Christina", "Eden", "Karla", "Lindsey", "Aga", "Chad", "Jenn", "Sarah"], lengthInMinutes: 10, theme: .poppy)
     ]
 }
