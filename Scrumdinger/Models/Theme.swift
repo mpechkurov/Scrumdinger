f60f71ee5695d353c49b5a711f1885c5950a98bb
Mykhailo Pechkurov <mihasyk1@gmail.com>
2022-01-16 22:14:18 +0100

    Display list of daily scrums

--- Scrumdinger/Models/Theme.swift
+++ Scrumdinger/Models/Theme.swift
@@ -28,8 +28,8 @@ enum Theme: String {
     
     var accentoColor: Color {
         switch self {
-            case .bubblegum, .buttercup, .lavender, .orange, .periwinkle, .poppy, .seafoam, .sky, .tan, .teal, .yellow: return .black
-            case .indigo, .magenta, .navy, .oxblood, .purple: return .white
+        case .bubblegum, .buttercup, .lavender, .orange, .periwinkle, .poppy, .seafoam, .sky, .tan, .teal, .yellow: return .black
+        case .indigo, .magenta, .navy, .oxblood, .purple: return .white
         }
     }
     var mainColor: Color {
