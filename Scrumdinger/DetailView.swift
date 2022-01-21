6814e35eeb6ce6f77e2302a59c11b9f2309746c0
Mykhailo Pechkurov <mihasyk1@gmail.com>
2022-01-21 22:10:12 +0100

    Created a Detail View

---
+++ Scrumdinger/DetailView.swift
@@ -0,0 +1,24 @@
+//
+//  DetailView.swift
+//  Scrumdinger
+//
+//  Created by Misha Pechkurov on 21.01.22.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    let scrum: DailyScrum
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            DetailView(scrum: DailyScrum.sampleData[0])
+        }
+    }
+}
